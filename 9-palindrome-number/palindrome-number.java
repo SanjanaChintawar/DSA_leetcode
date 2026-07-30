e65583d4fa79a51a8c002f7f7065409fd6157130
Sanjana Sachin Chintawar <154829333+SanjanaChintawar@users.noreply.github.com>
2026-07-30 21:13:33 +0530

    Time: 5 ms (80.83%) | Memory: 45.9 MB (53.53%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int y = x;
+        int rev = 0;
+
+        if(x < 0){
+            return false;
+        }
+
+        while(x!=0){
+            int digit = x%10;
+            x /= 10;
+
+            rev = rev *10 + digit;
+        }
+
+        if(rev == y){
+            return true;
+        }
+        return false;
+    }
+}
